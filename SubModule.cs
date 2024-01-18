6e7b32615c0b2d529fb00a71937b61dfd64392a6
Enis Necipoglu <iletisim@enisnecipoglu.com>
2024-01-19 00:34:04 +0300

    Migrate to new localization logic

--- SubModule.cs
+++ SubModule.cs
@@ -6,13 +6,11 @@ public class SubModule : MBSubModuleBase
     protected override void OnSubModuleLoad()
     {
         base.OnSubModuleLoad();
-
     }
 
     protected override void OnSubModuleUnloaded()
     {
         base.OnSubModuleUnloaded();
-
     }
 
     protected override void OnBeforeInitialModuleScreenSetAsRoot()
